a7775f631468d64a01a01a36d5ed8967c89c4cff
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-07-31 18:32:52 +0530

    add longest palindromic substring

---
+++ Algo Expert/Strings/longest_palindromic_substring.py
@@ -0,0 +1,14 @@
+def longestPalindromicSubstring(string):
+    output = ""
+    n = len(string)
+    if n == 1:
+        return string
+    for i in range(0, n - 1):
+        for j in range(i + 1, n):
+            if string[i: j + 1] == string[i: j + 1][::-1]:
+                output = string[i: j + 1] if j - \
+                    i + 1 >= len(output) else output
+    return output
+
+
+print(longestPalindromicSubstring("a"))

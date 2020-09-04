df577d40708022ea782ac3d2a62d0a2c29fbe1c4
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-09-04 18:55:01 +0530

    add contains duplicate 2

---
+++ LeetCode/Arrays/contains_duplicate_2.py
@@ -0,0 +1,9 @@
+# Question - https://leetcode.com/problems/contains-duplicate-ii/submissions/
+
+class Solution:
+    def containsNearbyDuplicate(self, nums, k):
+        seen = {}
+        for i, x in enumerate(nums): 
+            if x in seen and i - seen[x] <= k: return True 
+            seen[x] = i
+        return False 
